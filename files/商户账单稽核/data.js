8fb9e3ec2c2fd79b1425d51a1df64ac3ff868c2d
unrulyaprilday-eng <unrulyaprilday@gmail.com>
2026-05-14 18:11:51 +0800

    1

---
+++ files/商户账单稽核/data.js
@@ -0,0 +1,31 @@
+$axure.loadCurrentPage(
+(function() {
+  return {
+    "url": "商户账单稽核.html",
+    "generationDate": new Date(2026, 4, 14),
+    "isCanvasEnabled": false,
+    "variables": ["OnLoadVariable"],
+    "page": {
+      "packageId": "merchant_bill_audit",
+      "type": "Axure:Page",
+      "name": "商户账单稽核",
+      "notes": {},
+      "annotations": [],
+      "style": {},
+      "diagram": {
+        "objects": []
+      }
+    },
+    "masters": {},
+    "objectPaths": {},
+    "defaultAdaptiveView": {
+      "name": "",
+      "size": {
+        "width": 0,
+        "height": 0
+      },
+      "condition": ""
+    },
+    "adaptiveViews": []
+  };
+})());
